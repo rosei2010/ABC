64d12bab2a54a6747c8a00c8cb178a03e445c1cd
Richard Osei <richardosei25@gmail.com>
2022-09-02 14:11:34 +0100

    project

--- main.tf
+++ main.tf
@@ -40,6 +40,17 @@ resource "aws_subnet" "private-sub1" {
   }
 }
 
+# Private subnet
+resource "aws_subnet" "private-sub2" {
+  vpc_id     = aws_vpc.Kane-vpc.id
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "private-sub2"
+  }
+}
+
+
 
 # Public route table
 resource "aws_route_table" "public-route-table" {
@@ -96,3 +107,25 @@ resource "aws_route" "Public-igw-route" {
   destination_cidr_block    = "0.0.0.0/0"
 }
 
+
+# EIP 
+resource "aws_eip" "Elastic-IP-1" {
+  vpc = true
+  associate_with_private_ip = "3.11.1.226"
+  depends_on                = [aws_internet_gateway.IGW]
+}
+
+
+  # Nat gateway
+  resource "aws_nat_gateway" "July-nat-gw" {
+  allocation_id = aws_eip.Elastic-IP-1.id
+  subnet_id     = aws_subnet.public-sub1.id
+
+  tags = {
+    Name = "July-nat-gw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.IGW]
+}
